1ae9818ff37786a480e12f028ff83aa0bc0d0507
uyanda-maseti <masetibabalo2@gmail.com>
2025-05-24 17:45:51 +0200

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id: "cShirt1",
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg"
+},{
+    id: "dgheriuy",
+    name: "Office Shirt",
+    price: 300,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg"
+},{
+    id: "vhfgyiu",
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg"
+},{
+    id: "dfgghjk",
+    name: "Mens Suit",
+    price: 1000,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg"
+}]
